c29e1d748a56f1843ecb9cf79deac4f9dd8fa256
Rafael Bandeira dos Santos <rafael.bandeiradossantos-ext@ab-inbev.com>
2021-07-07 00:13:45 -0300

    created bank account class and pre started its members

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Account.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Account(
+    val agency: String,
+    val id: String,
+    val saldo: BigDecimal
+) {
+    fun deposit(amount: BigDecimal) {
+
+    }
+
+    fun takeOut(amount: BigDecimal) {
+
+    }
+}
