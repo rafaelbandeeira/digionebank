b1b1e9012a82474ddf44d1190e61a68d42a17a6e
Rafael Bandeira dos Santos <rafael.bandeiradossantos-ext@ab-inbev.com>
2021-07-09 01:44:43 -0300

    add auth parameter to manager

--- src/main/kotlin/one/digitalinnovation/digionebank/Manager.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Manager.kt
@@ -3,7 +3,8 @@ package one.digitalinnovation.digionebank
 class Manager(
     name: String,
     cpf: String,
-    salary: Double
+    salary: Double,
+    val auth: String
 ) : Employee(name, cpf, salary){
     override fun calcStimulus(): Double = salary * 0.1
 }
